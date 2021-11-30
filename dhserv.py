6e007cf172c78b6dd7e8f9cec881d2b88cfcd304
aveabina <91433112+aveabina@users.noreply.github.com>
2021-12-01 02:41:32 +0300

    Update dhserv.py

--- dhserv.py
+++ dhserv.py
@@ -1,14 +1,16 @@
 import socket
+
 HOST = '127.0.0.1'
 PORT = 8080
 sock = socket.socket()
 sock.bind((HOST, PORT))
 sock.listen(1)
 c, a = sock.accept()
-allowed_keys = [5, 7, 13, 15, 157, 199]
+allowed_keys = [3, 17, 151, 197]
 
 connect = 'Connect: ' + str(a)
 print(connect)
+
 fi = open('ports.txt','a+')
 fi.write('\n' + connect + '; ')
 fi.close() 
@@ -22,25 +24,24 @@ while True:
 new_data = data.split(' ')
 g = int(new_data[0])
 p = int(new_data[1])
-a = int(new_data[2])
-A = int(new_data[3])
+A = int(new_data[2])
 
-if a in allowed_keys:
-	b = int(input("Input b > "))
-	if b in allowed_keys:
+if (g in allowed_keys) and (p in allowed_keys):
+	
+	b = int(input("Input b > "))		
+	
+	file = open('keys.txt', 'a+')
+	file.write('\ Private key b: ' + str(b))
+	file.close()
+	
+	B = pow(g, b) % p
+	c.send(str(B).encode('utf-8'))
+	c.close()
 		
-		file = open('keys.txt', 'a+')
-		file.write('\ Private key b: ' + str(b))
-		B = pow(g, b) % p
-		c.send(str(B).encode('utf-8'))
-		c.close()
-		file.close()
-		
-		K = pow(A, b) % p
-		print("Key > " + str(K))
-		input("Done.")
-		file.close()
-	else:
-		print('Unresolved private key!')
+	K = pow(A, b) % p
+	print("Key > " + str(K))
+	input("Done.")
+	
 else:
 	print('Unresolved private key!')
+
