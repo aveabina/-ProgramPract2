534c5c98c184f32328c5043fe0a72cb4bed19ec3
aveabina <91433112+aveabina@users.noreply.github.com>
2021-12-22 18:39:24 +0300

    Update test.py

--- 8.Flaskapp/test.py
+++ 8.Flaskapp/test.py
@@ -1,24 +1,20 @@
 import requests
-
 print('>User creation')
 response = requests.put(
     'httplocalhost7070userbina',
-    {'password' 'bn2809'}
-)
+    {'password' 'bn2809'})
 print(response.json())
 
 print('>Adding user with same password')
 response = requests.put(
     'httplocalhost7070userlili',
-    {'password' 'bn2809'}
-)
+    {'password' 'bn2809'})
 print(response.json())
 
 print('>Adding user with existing name')
 response = requests.put(
     'httplocalhost7070userbina',
-    {'password' 'bn2usr'}
-)
+    {'password' 'bn2usr'})
 print(response.json())
 
 print('>Get user info')
